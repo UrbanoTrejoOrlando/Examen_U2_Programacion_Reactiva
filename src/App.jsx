b982f9c37900f2093882b3ecd771cfa6fd253f53
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-04-25 09:37:21 -0600

    Modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <MovieProvider>
     <div className="max-w-2xl mx-auto p-6">
-    <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">Lista Peliculas</h1>
+    <h1 className="text-3xl font-bold text-center mb-6 text-red-700">Lista Peliculas</h1>
     <Formulario></Formulario>
     <MovieFilters></MovieFilters>
     <FavoriteList></FavoriteList>
